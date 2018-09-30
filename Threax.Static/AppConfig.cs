f97f51002adc4ae3d0b03b31c23d297bee36951c
Andrew Piper <andrew@threax.com>
2018-09-30 08:31:22 -0400

    Added docker compatability.

--- Threax.Static/AppConfig.cs
+++ Threax.Static/AppConfig.cs
@@ -8,6 +8,12 @@ namespace Threax.Static
     public class AppConfig
     {
         /// <summary>
+        /// The base path the app lives on. Used for cookie paths and to enforce the url spelling.
+        /// Can be null to live on the root path.
+        /// </summary>
+        public string PathBase { get; set; }
+
+        /// <summary>
         /// If the file is not a recognized content-type should it be served? Default: false.
         /// </summary>
         public bool ServeUnknownFileTypes { get; set; } = false;
--- Threax.Static/Startup.cs
+++ Threax.Static/Startup.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Threax.AspNetCore.BuiltInTools;
 using Threax.Extensions.Configuration.SchemaBinder;
 
@@ -30,6 +32,8 @@ namespace Threax.Static
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Threax.AspNetCore.Docker.Certs.CertManager.LoadTrustedRoots(o => Configuration.Bind("CertManager", o));
+
             services.AddScoped<IToolRunner>(s =>
             {
                 return new ToolRunner()
@@ -42,9 +46,24 @@ namespace Threax.Static
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseHttpsRedirection();
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedProto
+                //Can add ForwardedHeaders.XForwardedFor later, but tricky with container proxy since we don't know its ip
+                //This is enough to get https detection working again, however.
+                //https://github.com/aspnet/Docs/issues/2384
+            });
+
+            app.UseUrlFix(o =>
+            {
+                o.CorrectPathBase = appConfig.PathBase;
+            });
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             app.UseDefaultFiles();
             app.UseStaticFiles(new StaticFileOptions()
             {
